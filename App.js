fc15379e34807eca1a3e08d0c208ec4087562245
filemon52 <42663325+filemon52@users.noreply.github.com>
2018-08-24 12:24:23 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Filemon Steven Juliyanto</Text>
+        <Text>XI RPL 2</Text>
+        <Text>19</Text>
+       </View>
+    );
+  }
+}	
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
